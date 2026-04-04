7c0befaaa13a3a005cf9b6f84a37e632b4e6ffcc
Brian Morris <contact@brianmorris.info>
2026-04-03 22:12:18 -0400

    chore(functions): upgrade firebase-functions and other firebase function dependencies

--- functions/index.js
+++ functions/index.js
@@ -6,16 +6,17 @@ const pdfResumePath = '/brianmorris.pdf';
 const vCardPath = '/brianmorris.vcf';
 const profilePath = '/img/Brian-Morris.jpg';
 
-const functions = require('firebase-functions');
+const { onRequest } = require('firebase-functions/v2/https');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views', 'pages'));
 
 app.get('/', (request, response) => {
   response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
-  response.render(path.join(__dirname, 'views', 'pages', 'home'));
+  response.render('home');
 });
 
 app.get("/*contact*", (request, response) => {
@@ -174,4 +175,4 @@ app.get("/*", (request, response) => {
   response.redirect("/#contact");
 });
 
-exports.app = functions.https.onRequest(app);
+exports.app = onRequest(app);
